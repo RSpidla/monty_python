f6c2ecd30ec0625978d1b2b82321d64a4cbd853b
‘RSpidla’ <rspidla@gmail.com>
2019-02-09 22:14:10 +0000

    InitialzeD variable and required node https module

--- https_example.js
+++ https_example.js
@@ -1,2 +1,7 @@
 // GOAL of EXCERCISE - To further practice working with streams in NodeJS as well as begin using the http (https) module to make simple GET requests for data.
 
+
+// STEP 1 - Initialze variable and require node https module
+const https = require('https');
+
+console.log('I did it!');
