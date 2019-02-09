b631482379e1fcce4bce32aabd3f0482c6053b2b
‘RSpidla’ <rspidla@gmail.com>
2019-02-09 22:49:39 +0000

    Switched options variable to send back entire Monty Python script

--- https_example.js
+++ https_example.js
@@ -1,28 +1,34 @@
 // GOAL of EXCERCISE - To further practice working with streams in NodeJS as well as begin using the http (https) module to make simple GET requests for data.
 
 
-// STEP 1 - Initialze variable and require node https module
-var https = require('https');
+// STEP 1 - Initialze variable and set it's value to require node https module
+const https = require('https');
 
-// STEP 2 - 
-var options = {
-    host: 'www.example.org',
-    path: '/'
+// STEP 2 - Initialize variable and set node https module options
+  // Option 1 - URL
+  // Option 2 - Path
+const options = {
+    host: 'stream-large-file.herokuapp.com',
+    path: '/give-me-stuff-now'
 };
 
-// STEP 3 - 
-// called by https when the request is made.
-var callback = function(response) {
-    console.log('In response handler callback!');
-    // console.log('Response: ', response);
+// STEP 3 - Declare function expression which is called by https when the request is made.
+let callback = function(response) {
+  console.log('In response handler callback!');
 
-    response.on('data', function(chunk) {
-        console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
-    });
-  }
-  
-  console.log("I'm about to make the request!");
-  
-  https.request(options, callback).end();
-  
-  console.log("I've made the request!");
+  // STEP 4 - Output data as it comes in.
+    // Label response data as 'chunk'
+    // Print chunk.length
+  response.on('data', function(chunk) {
+      console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+  });
+}
+
+// STEP 5 - Print to standard output 'I'm about to make the request!'.
+console.log("I'm about to make the request!");
+
+// STEP 6 - Make request to secure web server
+https.request(options, callback).end();
+
+// STEP 7 - Print to standard output 'I've made the request!'.
+console.log("I've made the request!");
